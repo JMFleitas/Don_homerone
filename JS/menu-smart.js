c26a2b2c963f43c89f493723772e050913581752
Jonathan Fleitas <marcelofolk17@gmail.com>
2024-02-02 21:09:55 -0300

    agrega pagina de menu

---
+++ JS/menu-smart.js
@@ -0,0 +1,21 @@
+const d = document;
+
+export default function menuSmart() {
+    const $imgSmart = d.querySelectorAll("img");
+
+
+    const cb = (entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            } else {
+                entry.target.classList.remove("visible");
+
+            }
+        })
+    };
+
+    const observer = new IntersectionObserver(cb, { threshold: 0.5 });
+
+    $imgSmart.forEach(el => observer.observe(el));
+}
--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,5 @@
 import slider from "./carrusel_index.js";
+import menuSmart from "./menu-smart.js";
 import menuToggle from "./menu_toggle.js";
 
 
@@ -7,4 +8,5 @@ const d = document;
 d.addEventListener("DOMContentLoaded", e => {
     menuToggle(".cta-btn", ".menu-toggle", ".menu-toggle a")
     slider();
+    menuSmart();
 })
